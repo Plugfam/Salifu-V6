fcb706e9ce545e7109a4f7343e76c25737cf0d16
Plugfam <Karimsalifu2740@gmail.com>
2025-06-04 13:58:29 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+/*
+┏━━━━━━━━━━━━━━━┓  
+┃ REPAA BASE - WHATSAPP     
+┣━━━━━━━━━━━━━━━┛
+┃♕ Creator: repaasangee           
+┃♕ AI Helper: ChatGPT             
+┃♔ Version: 2.0.2                   
+┗━━━━━━━━━━━━━━━┛
+*/
+//========REPAA========
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["233559278841"] 
+global.namabot = 'SALIFU-V6'
+//======================
+global.mess = { 
+owner: 'wow!, you are not my owner, brother',
+premium: 'you are not a premium user',
+succes: 'done bang'
+}
+//======================
